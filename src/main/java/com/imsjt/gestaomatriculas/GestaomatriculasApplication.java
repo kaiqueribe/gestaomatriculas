c1116ce1138c4144128faacecf06400a6ee180fc
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-06-25 13:31:31 -0300

    Criação Projeto ControleMatriculas

---
+++ src/main/java/com/imsjt/gestaomatriculas/GestaomatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.imsjt.gestaomatriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaomatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaomatriculasApplication.class, args);
+	}
+
+}
