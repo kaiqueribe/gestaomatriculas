a01a76d29aae89b1e0df39bf1aff7a006c95ff81
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-03 17:43:42 -0300

    refactor/service-crud

--- src/main/java/com/imsjt/gestaomatriculas/service/AtendidoService.java
+++ src/main/java/com/imsjt/gestaomatriculas/service/AtendidoService.java
@@ -1,4 +1,31 @@
 package com.imsjt.gestaomatriculas.service;
 
+import com.imsjt.gestaomatriculas.entity.Atendido;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AtendidoService {
+
+    public Atendido cadastrarAtendido(Atendido atendido) {
+        return null;
+    }
+
+    public Atendido buscarTodosAtendidos() {
+        return null;
+    }
+
+    public Atendido buscarAtendidoPorId(Long id) {
+        return null;
+    }
+
+    public Atendido atualizarAtendido(Long id, Atendido atendido) {
+        return null;
+    }
+
+    public void deletarAtendido(Long id) {
+
+    }
+
+
+
 }
