6ea59f35add5980dcc6266ced3f80fb8113bd049
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-03 17:45:24 -0300

    refactor/controller-crud

--- src/main/java/com/imsjt/gestaomatriculas/controller/AtendidoController.java
+++ src/main/java/com/imsjt/gestaomatriculas/controller/AtendidoController.java
@@ -1,4 +1,39 @@
 package com.imsjt.gestaomatriculas.controller;
 
+import com.imsjt.gestaomatriculas.entity.Atendido;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/atendido")
 public class AtendidoController {
+
+
+    @PostMapping
+    public ResponseEntity<Atendido> cadastrar(){
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<Atendido> buscarTodos(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Atendido> buscarPorId(){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Atendido> atualizar(){
+        return null;
+    }
+
+    @DeleteMapping
+    public void deletar(){
+
+    }
+
+
+    
 }
