7454f0876c70e74e3b0f4b1dc590350e0e0f6a64
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-04 17:32:13 -0300

    Criação NotFoundException

---
+++ src/main/java/com/imsjt/gestaomatriculas/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.imsjt.gestaomatriculas.exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
+
+public class NotFoundException extends HttpClientErrorException {
+    public NotFoundException(String text) {
+        super(HttpStatus.NOT_FOUND, text);
+    }
+
+}
