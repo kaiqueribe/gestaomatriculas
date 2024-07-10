ed346cf2426e420d2fd24aed2ad2028263fb3dae
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-10 10:33:21 -0300

    refactor Atendido Controller

--- src/main/java/com/imsjt/gestaomatriculas/controller/AtendidoController.java
+++ src/main/java/com/imsjt/gestaomatriculas/controller/AtendidoController.java
@@ -16,35 +16,36 @@ public class AtendidoController {
 
 
     @PostMapping
-    public ResponseEntity<Atendido> cadastrar(@RequestBody Atendido atendido){
+    public ResponseEntity<Atendido> cadastrar(@RequestBody Atendido atendido) {
         Atendido novoAtendido = atendidoService.cadastrarAtendido(atendido);
         return new ResponseEntity<>(novoAtendido, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Atendido>> buscarTodos(){
-       List<Atendido> atendidos = atendidoService.buscarTodosAtendidos();
-       return ResponseEntity.ok(atendidos);
+    public ResponseEntity<List<Atendido>> buscarTodos() {
+        List<Atendido> atendidos = atendidoService.listarTodosAtendidos();
+        return ResponseEntity.ok(atendidos);
 
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Atendido> buscarPorId(@PathVariable Long id){
-       Atendido atendido = atendidoService.buscarAtendidoPorId(id);
-       return ResponseEntity.ok(atendido);
+    public ResponseEntity<Atendido> buscarPorId(@PathVariable Long id) {
+        Atendido atendido = atendidoService.buscarAtendidoPorId(id);
+        return ResponseEntity.ok(atendido);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Atendido> atualizar(@PathVariable Long id, @RequestBody Atendido atendido){
-        Atendido atendidoAtualizado = atendidoService.atualizarAtendido(id,atendido);
+    public ResponseEntity<Atendido> atualizar(@PathVariable Long id, @RequestBody Atendido atendido) {
+        Atendido atendidoAtualizado = atendidoService.atualizarAtendido(id, atendido);
         return ResponseEntity.ok(atendidoAtualizado);
     }
 
-    @DeleteMapping
-    public void deletar(){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> remover(@PathVariable Long id) {
+        atendidoService.deletarAtendido(id);
+        return ResponseEntity.ok("Atendido deletado com sucesso!");
 
     }
 
 
-    
 }
