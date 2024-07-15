b77c6eac22013aa8268c72d4ad444460b373ff4e
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-15 14:58:20 -0300

    Alteração documentação

--- src/main/java/com/imsjt/gestaomatriculas/GestaomatriculasApplication.java
+++ src/main/java/com/imsjt/gestaomatriculas/GestaomatriculasApplication.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@OpenAPIDefinition(info = @Info(title = "IMSJT", version = "1", description = "API para gestão de matrículas"))
+@OpenAPIDefinition(info = @Info(title = "Gestão Matriculas", version = "1", description = "API para gestão de matrículas"))
 @SpringBootApplication
 public class GestaomatriculasApplication {
 
