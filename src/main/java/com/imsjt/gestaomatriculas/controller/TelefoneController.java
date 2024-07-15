a4ee73a4eb00692deec0efc77a859e5f70c0964c
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-15 14:54:16 -0300

    refactor metodo cadastrar telefone (Controller/Service/Repository)

--- src/main/java/com/imsjt/gestaomatriculas/controller/TelefoneController.java
+++ src/main/java/com/imsjt/gestaomatriculas/controller/TelefoneController.java
@@ -2,45 +2,52 @@ package com.imsjt.gestaomatriculas.controller;
 
 import com.imsjt.gestaomatriculas.entity.Telefone;
 import com.imsjt.gestaomatriculas.service.TelefoneService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/telefones")
-
+@AllArgsConstructor
 public class TelefoneController {
 
     //TODO: Implementar TelefoneController
-    private TelefoneService telefoneService;
+    private final TelefoneService telefoneService;
 
 
     @PostMapping
-    public ResponseEntity<Telefone> cadastrar(@RequestBody Telefone telefone){
-        var novoTelefone = telefoneService.cadastrarTelefone(telefone);
-        return new ResponseEntity<>(novoTelefone, HttpStatus.CREATED);
+    public ResponseEntity<Telefone> cadastrar(@RequestBody Telefone telefone) {
+
+        var telefoneCadastrado = telefoneService.cadastrarTelefone(telefone);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{numeroTelefone}").buildAndExpand(telefoneCadastrado.getNumeroTelefone()).toUri();
+        return ResponseEntity.created(uri).body(telefoneCadastrado);
     }
 
-    @GetMapping
-    public ResponseEntity<List<Telefone>> buscarTodos(){
-        return null;
-    }
-
-    public ResponseEntity<Telefone> buscarPorId(Long id){
-        return null;
-    }
-    //TODO: Implementar o método de atualizar
-    @PutMapping("/{id}")
-    public ResponseEntity<Telefone> atualizar(Long id,Telefone telefone){
-        return null;
-    }
-    public void remover(Long id){
-
-    }
-
-
-
+//    @GetMapping
+//    public ResponseEntity<List<Telefone>> buscarTodos() {
+//        return null;
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Telefone> buscarPorId(Long id) {
+//        return null;
+//    }
+//
+//    //TODO: Implementar o método de atualizar
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Telefone> atualizar(Long id, Telefone telefone) {
+//        return null;
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void remover(Long id) {
+//
+//    }
 
 }
--- src/main/java/com/imsjt/gestaomatriculas/service/TelefoneService.java
+++ src/main/java/com/imsjt/gestaomatriculas/service/TelefoneService.java
@@ -2,13 +2,14 @@ package com.imsjt.gestaomatriculas.service;
 
 import com.imsjt.gestaomatriculas.entity.Telefone;
 
+import com.imsjt.gestaomatriculas.exceptions.InvalidRequestException;
 import com.imsjt.gestaomatriculas.repository.TelefoneRepository;
-import com.sun.jdi.request.InvalidRequestStateException;
+
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -17,29 +18,32 @@ public class TelefoneService {
     private TelefoneRepository telefoneRepository;
 
     //TODO: Implementar TelefoneService
-
     public Telefone cadastrarTelefone(Telefone telefone) {
-        telefoneRepository.findByNumeroTelefone(telefone.getNumeroTelefone()).ifPresent(numeroTelefone -> {
-            throw new InvalidRequestStateException("Telefone Já Cadastrado!"+ telefone.getNumeroTelefone());
+
+        telefoneRepository.findByNumeroTelefone(telefone.getNumeroTelefone())
+                .ifPresent(numeroTelefone -> {
+            throw new InvalidRequestException("Telefone Já Cadastrado!"+ telefone.getNumeroTelefone());
         });
-        Telefone novoTelefone = telefoneRepository.save(telefone);
-        return telefone;
-    }
 
-    public List<Telefone> listarTodosTelefones() {
-        return null;
-    }
-
-    public Telefone buscarTelefonePorId(Long id) {
-        return null;
-    }
-
-    public Telefone atualizarTelefone(Long id, Telefone telefone) {
-        return null;
-    }
-
-    public void deletarTelefone(Long id) {
+        var novoTelefone = telefoneRepository.save(telefone);
+        return novoTelefone;
 
     }
+
+//    public List<Telefone> listarTodosTelefones() {
+//        return null;
+//    }
+//
+//    public Telefone buscarTelefonePorId(Long id) {
+//        return null;
+//    }
+//
+//    public Telefone atualizarTelefone(Long id, Telefone telefone) {
+//        return null;
+//    }
+//
+//    public void deletarTelefone(Long id) {
+//
+//    }
 
 }
