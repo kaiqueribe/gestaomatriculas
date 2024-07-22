5c878ce54aa70e79ca8f8ea7c063aed552d0e30e
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-22 10:18:53 -0300

    refactor Repositories

--- src/main/java/com/imsjt/gestaomatriculas/repository/AtendidoRepository.java
+++ src/main/java/com/imsjt/gestaomatriculas/repository/AtendidoRepository.java
@@ -2,7 +2,7 @@ package com.imsjt.gestaomatriculas.repository;
 
 import com.imsjt.gestaomatriculas.entity.Atendido;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
--- src/main/java/com/imsjt/gestaomatriculas/repository/EnderecoRepository.java
+++ src/main/java/com/imsjt/gestaomatriculas/repository/EnderecoRepository.java
@@ -4,6 +4,6 @@ import com.imsjt.gestaomatriculas.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
 }
