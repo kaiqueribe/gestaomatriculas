36fc2a7f84c5361a5e4bfbf5873a932c361987a3
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-07-22 10:22:32 -0300

    refactor enderecoService

--- src/main/java/com/imsjt/gestaomatriculas/service/EnderecoService.java
+++ src/main/java/com/imsjt/gestaomatriculas/service/EnderecoService.java
@@ -1,25 +1,22 @@
 package com.imsjt.gestaomatriculas.service;
 
-import com.imsjt.gestaomatriculas.entity.Atendido;
 import com.imsjt.gestaomatriculas.entity.Endereco;
 import com.imsjt.gestaomatriculas.exceptions.NotFoundException;
 import com.imsjt.gestaomatriculas.repository.EnderecoRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class EndereçoService {
+public class EnderecoService {
 
     private EnderecoRepository enderecoRepository;
 
     //TODO Implementar todos os metodos com as regras de negocio
     public Endereco cadastrarEndereco(Endereco endereco) {
-
-        var novoEndereco = enderecoRepository.save(endereco);
+        Endereco novoEndereco = enderecoRepository.save(endereco);
         return novoEndereco;
     }
 
@@ -32,10 +29,11 @@ public class EndereçoService {
         Endereco endereco = enderecoRepository.findById(id).orElseThrow(() -> new NotFoundException("Endereco com id: " + id + " não encontrado!"));
         return endereco;
     }
+
     //TODO Implementar atualizar EnderecoService
-    public Atendido atualizarEndereco(Long id, Endereco endereco){
-        return null;
-    }
+//    public Atendido atualizarEndereco(Long id, Endereco endereco) {
+//        return null;
+//    }
 
 
     public void deletarEndereco(Long id) {
