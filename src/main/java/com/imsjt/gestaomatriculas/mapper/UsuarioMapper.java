1a6deca0609042a69f52f4add94498425077b80b
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-09-04 16:12:37 -0300

    refactor UsuarioMapper

--- src/main/java/com/imsjt/gestaomatriculas/mapper/UsuarioMapper.java
+++ src/main/java/com/imsjt/gestaomatriculas/mapper/UsuarioMapper.java
@@ -2,7 +2,10 @@ package com.imsjt.gestaomatriculas.mapper;
 
 import com.imsjt.gestaomatriculas.dto.UsuarioDTO;
 import com.imsjt.gestaomatriculas.entity.Usuario;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
+@Mapper
 public interface UsuarioMapper {
     UsuarioDTO toDTO(Usuario entity);
 
