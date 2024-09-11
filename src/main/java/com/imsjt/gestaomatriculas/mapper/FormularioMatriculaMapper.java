84cdda34bd8e6f4e66b49c715d86b46d3a9e7d0b
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-09-11 12:01:19 -0300

    refactor(mapper): Alterando FormularioMapper

--- src/main/java/com/imsjt/gestaomatriculas/mapper/FormularioMatriculaMapper.java
+++ src/main/java/com/imsjt/gestaomatriculas/mapper/FormularioMatriculaMapper.java
@@ -1,10 +1,12 @@
 package com.imsjt.gestaomatriculas.mapper;
 
-import com.imsjt.gestaomatriculas.dto.FormularioMatriculaDTO;
+import com.imsjt.gestaomatriculas.dto.MatriculaDTO;
 import com.imsjt.gestaomatriculas.entity.FormularioMatricula;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface FormularioMatriculaMapper {
-    FormularioMatriculaDTO toDTO(FormularioMatricula entity);
+    MatriculaDTO toDTO(FormularioMatricula entity);
 
-    FormularioMatricula toEntity(FormularioMatriculaDTO formularioMatriculaDTO);
+    FormularioMatricula toEntity(MatriculaDTO matriculaDTO);
 }
