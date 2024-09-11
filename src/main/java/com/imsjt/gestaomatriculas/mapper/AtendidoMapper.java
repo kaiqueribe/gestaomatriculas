903ad37dd3a1c948681128a0f02b51a9b7441521
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-09-11 11:45:20 -0300

    refactor(mapper): Inserindo Anotação na classe Atendido Mapper

--- src/main/java/com/imsjt/gestaomatriculas/mapper/AtendidoMapper.java
+++ src/main/java/com/imsjt/gestaomatriculas/mapper/AtendidoMapper.java
@@ -2,7 +2,9 @@ package com.imsjt.gestaomatriculas.mapper;
 
 import com.imsjt.gestaomatriculas.dto.AtendidoDTO;
 import com.imsjt.gestaomatriculas.entity.Atendido;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface AtendidoMapper {
     AtendidoDTO toDTO(Atendido entity);
 
