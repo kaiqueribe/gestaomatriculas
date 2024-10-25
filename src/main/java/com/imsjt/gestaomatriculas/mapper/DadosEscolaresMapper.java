1b65039f9b87adf29d90554bef5aef5894dfa175
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-10-24 23:02:46 -0300

    style(mapper): Pequenos ajustes organização do codigo mapper

--- src/main/java/com/imsjt/gestaomatriculas/mapper/DadosEscolaresMapper.java
+++ src/main/java/com/imsjt/gestaomatriculas/mapper/DadosEscolaresMapper.java
@@ -2,7 +2,9 @@ package com.imsjt.gestaomatriculas.mapper;
 
 import com.imsjt.gestaomatriculas.dto.DadosEscolaresDTO;
 import com.imsjt.gestaomatriculas.entity.DadosEscolares;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface DadosEscolaresMapper {
     DadosEscolaresDTO toDTO(DadosEscolares entity);
 
