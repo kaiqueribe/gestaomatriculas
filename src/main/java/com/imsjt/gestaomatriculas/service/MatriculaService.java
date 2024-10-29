34945bb3894fbd09ad6cceab81e7c89dfdf4dc32
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-10-29 10:45:12 -0300

    refactor(matricula): modificando MatriculaService

--- src/main/java/com/imsjt/gestaomatriculas/service/MatriculaService.java
+++ src/main/java/com/imsjt/gestaomatriculas/service/MatriculaService.java
@@ -1,26 +1,20 @@
 package com.imsjt.gestaomatriculas.service;
 
-import com.imsjt.gestaomatriculas.dto.AtendidoDTO;
-import com.imsjt.gestaomatriculas.dto.EnderecoDTO;
-import com.imsjt.gestaomatriculas.dto.MatriculaDTO;
-import com.imsjt.gestaomatriculas.entity.Atendido;
-import com.imsjt.gestaomatriculas.entity.Endereco;
-import com.imsjt.gestaomatriculas.entity.Matricula;
-import com.imsjt.gestaomatriculas.entity.Responsavel;
-import com.imsjt.gestaomatriculas.exceptions.InvalidRequestException;
-import com.imsjt.gestaomatriculas.exceptions.NotFoundException;
-import com.imsjt.gestaomatriculas.mapper.AtendidoMapper;
-import com.imsjt.gestaomatriculas.mapper.EnderecoMapper;
-import com.imsjt.gestaomatriculas.mapper.MatriculaMapper;
-import com.imsjt.gestaomatriculas.repository.AtendidoRepository;
+import com.imsjt.gestaomatriculas.dto.*;
+import com.imsjt.gestaomatriculas.entity.*;
+
+import com.imsjt.gestaomatriculas.mapper.*;
+
 import com.imsjt.gestaomatriculas.repository.MatriculaRepository;
-import com.imsjt.gestaomatriculas.repository.ResponsavelRepository;
+
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -29,17 +23,22 @@ import java.util.Optional;
 public class MatriculaService {
 
     //TODO Unificar Logica de matricula de atendido nessa service matricula
-
+    @Autowired
     private MatriculaRepository matriculaRepository;
     @Autowired
     private AtendidoService atendidoService;
+    @Autowired
     private TelefoneService telefoneService;
+    @Autowired
     private ResponsavelService responsavelService;
+    @Autowired
     private EnderecoService enderecoService;
 
     private final MatriculaMapper matriculaMapper;
     private final AtendidoMapper atendidoMapper;
     private final EnderecoMapper enderecoMapper;
+    private final TelefoneMapper telefoneMapper;
+    private final ResponsavelMapper responsavelMapper;
 
 
     public MatriculaDTO realizarMatricula(MatriculaDTO matriculaDTO) {
@@ -53,16 +52,24 @@ public class MatriculaService {
         dadosMatriculado.setAtendido(novoAtendido);
         log.info(" Cadastrou novo atendido ");
 
-        EnderecoDTO enderecoDTO = enderecoService.cadastrarEndereco(matriculaDTO.getEnderecoDTO());
-        Endereco novoEndereco = enderecoMapper.toEntity(enderecoDTO);
+        EnderecoDTO novoEnderecoDTO = enderecoService.cadastrarEndereco(matriculaDTO.getEnderecoDTO());
+        Endereco novoEndereco = enderecoMapper.toEntity(novoEnderecoDTO);
         dadosMatriculado.getAtendido().setEndereco(novoEndereco);
         log.info(" Cadastrou novo Endereco ");
 
+        TelefoneDTO telefoneDTO = telefoneService.cadastrarTelefone(matriculaDTO.getTelefoneDTOList().getFirst());
+        Telefone novoTelefone = telefoneMapper.toEntity(telefoneDTO);
+        List<Telefone> telefoneList = new ArrayList<>();
+        telefoneList.add(novoTelefone);
+        dadosMatriculado.getAtendido().setTelefonesList(telefoneList);
 
-        //TODO verifica se foi definido pelo menos um telefone
+        ResponsavelDTO responsavelDTO = responsavelService.cadastrarResponsavel(matriculaDTO.getResponsavelDTOList().getFirst());
+        Responsavel novoResponsavel = responsavelMapper.toEntity(responsavelDTO);
+        List<Responsavel> responsavelList = new ArrayList<>();
+        responsavelList.add(novoResponsavel);
+        dadosMatriculado.getAtendido().setResponsavelList(responsavelList);
 
-
-        Matricula novaMatricula =  matriculaRepository.save(dadosMatriculado);
+        Matricula novaMatricula = matriculaRepository.save(dadosMatriculado);
         return matriculaMapper.toDTO(novaMatricula);
     }
 
