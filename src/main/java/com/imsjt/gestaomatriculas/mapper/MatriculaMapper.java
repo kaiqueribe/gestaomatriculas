69bfb94f849775c3749d91afda90f59d2e574e40
Kaique Ribeiro <57017906+kaiqueribe@users.noreply.github.com>
2024-11-04 00:19:28 -0300

    refactor(mapper): modificando mapper para tentar corrigir retorno body json, solução não aplicada

--- src/main/java/com/imsjt/gestaomatriculas/mapper/MatriculaMapper.java
+++ src/main/java/com/imsjt/gestaomatriculas/mapper/MatriculaMapper.java
@@ -3,10 +3,17 @@ package com.imsjt.gestaomatriculas.mapper;
 import com.imsjt.gestaomatriculas.dto.MatriculaDTO;
 import com.imsjt.gestaomatriculas.entity.Matricula;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface MatriculaMapper {
     MatriculaDTO toDTO(Matricula entity);
 
     Matricula toEntity(MatriculaDTO matriculaDTO);
+
+//    @Mapping(source = "atendido", target = "id")
+//    @Mapping(source = "endereco", target = "id")
+//    @Mapping(source = "telefones", target = "id")
+//    @Mapping(source = "responsaveis", target = "id")
+//    MatriculaDTO toDTOComplete(Matricula matricula);
 }
